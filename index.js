f0113b4e22a3e45e1221b8c1491e5ee153a82bb5
McKayla Lankau <lankaukk@yahoo.com>
2021-08-16 14:54:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+superbowlWin = (record) => {
+    let result = record.find( function(s) { return s.result === "W" })
+    return !!result ? result.year : undefined
+}
